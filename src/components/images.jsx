c12959902690b422e0d9b4d34c268fc38b822071
salahkai <salah.kaifas@gmail.com>
2022-09-23 11:14:16 +0100

    Create images.jsx

---
+++ src/components/images.jsx
@@ -0,0 +1,7 @@
+              //<nav className="d-flex justify-content-start  ms-4"> 
+                // <div className="d-sm-flex">
+                 //  <img className="mx-4" height="100px" src={spe}></img>
+                 //  <img className="mx-4" height="100px" src={pco}></img>
+                 //  <img className="mx-4" height="100px" src={univ}></img>
+              //   </div>
+              // </nav>
