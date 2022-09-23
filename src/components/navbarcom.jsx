326c24b2cb25b8038e60e95b67f6e561ff49d0dc
salahkai <salah.kaifas@gmail.com>
2022-09-23 11:13:40 +0100

    Update navbarcom.jsx

--- src/components/navbarcom.jsx
+++ src/components/navbarcom.jsx
@@ -38,13 +38,7 @@ class NavBarCom extends React.Component {
 
             <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
             <Navbar.Collapse id="responsive-navbar-nav">
-              //<nav className="d-flex justify-content-start  ms-4">
-                // <div className="d-sm-flex">
-                 //  <img className="mx-4" height="100px" src={spe}></img>
-                 //  <img className="mx-4" height="100px" src={pco}></img>
-                 //  <img className="mx-4" height="100px" src={univ}></img>
-              //   </div>
-              // </nav>
+
 
               <Nav className="ms-auto">
                 <Nav.Link
